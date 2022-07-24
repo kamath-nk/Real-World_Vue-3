308ae2cd7b3c3a7e343fe93c26ade25c09f71f27
KamathNK <kamathnk@outlook.com>
2022-07-24 14:33:57 +0530

    Changes done as part chapter 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events for good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 1322423232,
+          category: 'Animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friends at this event.',
+          location: 'Meow Town',
+          date: 'January 26, 2023',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Cat Laydee'
+        },
+        {
+          id: 13224232,
+          category: 'Animal welfare',
+          title: 'Cat Adoption Day Feb',
+          description: 'Find your new feline friends at this event.',
+          location: 'Meow Town',
+          date: 'February 26, 2023',
+          time: '10:00',
+          petsAllowed: true,
+          organizer: 'Cat Laydee'
+        }
+      ]
+    }
   }
-};
+}
 </script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
